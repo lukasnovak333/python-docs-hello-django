7010996df8346031f4c99b71a7e1a7f2638f1820
lukas novak <lukas@lukas-mac.local>
2021-06-29 14:35:32 -0400

    debugging

--- hello/services.py
+++ hello/services.py
@@ -19,16 +19,27 @@ from django.views.decorators.csrf import csrf_exempt
 # Our content
 # from .models import *
 
-@csrf_exempt
-@require_http_methods(["POST"])
+# @csrf_exempt
+# @require_http_methods(["POST"])
 def single_service(request):
 	try:
+		if 'body' not in request:
+			return JsonResponse({'error': 'true', 'message': 'nobodyfound'})
+
 		content = request['body']
+
+		if 'conditionAndId' not in content:
+			return JsonResponse({'error': 'true', 'message': 'nocondandid'})
+			
 		cond_and_id = content['conditionAndId']
+
+		if 'finalResult' not in content:
+			return JsonResponse({'error': 'true', 'message': 'nocondandid'})
+
 		final_result = content['finalResult']
 
 
-		f= open("/home/site/textFiles/tmptest.txt","a")
+		f= open("/home/site/textFiles/tmptest.txt","w+")
 		f.write('----------------')
 		f.write('condition and Id - {}'.format(cond_and_id))
 		f.write('finalResult - {}'.format(final_result))
