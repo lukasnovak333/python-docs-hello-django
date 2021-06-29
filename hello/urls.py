475540c1fdf848713fea40c04d6771d9a8dcec60
lukas novak <lukas@lukas-mac.local>
2021-06-29 12:53:15 -0400

    fixed URL

--- hello/urls.py
+++ hello/urls.py
@@ -1,11 +1,8 @@
-import sys
-sys.path.append("..")
-
 from django.urls import path
 
 from . import views, services
 
 urlpatterns = [    
     path('', views.hello, name='hello'),
-    path('/services/postTestResult', services.single_service, name='service')
+    path('services/postTestResult', services.single_service, name='service')
 ]
