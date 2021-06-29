5544b27d0171e909ea0bbf9d77322b60488d8fcd
lukas novak <lukas@lukas-mac.local>
2021-06-29 13:45:36 -0400

    added posting functionality for final results to /home/site/textFiles

--- hello/services.py
+++ hello/services.py
@@ -19,7 +19,7 @@ from django.views.decorators.http import require_http_methods
 # from .models import *
 
 
-#@require_http_methods(["POST"])
+@require_http_methods(["POST"])
 def single_service(request):
 	""" This is a service, defined in urls.py, and 
 	    called via ajax methods (e.g. fetch)
@@ -38,8 +38,16 @@ def single_service(request):
 	    returns. 
 	"""
 
-	f= open("/home/site/textFiles/tmptest.txt","w+")
-	f.write('This is content! - modified in Home')
+	content = request.body
+	cond_and_id = content['conditionAndId']
+	final_result = content['finalResult']
+
+
+	f= open("/home/site/textFiles/tmptest.txt","a")
+	f.write('----------------')
+	f.write('condition and Id - {}'.format(cond_and_id))
+	f.write('finalResult - {}'.format(final_result))
+	f.write('----------------')
 	f.close()
 
 	return JsonResponse({'success': "true"})
