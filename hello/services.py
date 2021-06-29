87b59b8aded82a0fc8d3b7a75b5839a36e8872aa
lukas novak <lukas@lukas-mac.local>
2021-06-29 14:21:26 -0400

    debugging

--- hello/services.py
+++ hello/services.py
@@ -22,26 +22,11 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 @require_http_methods(["POST"])
 def single_service(request):
-	""" This is a service, defined in urls.py, and 
-	    called via ajax methods (e.g. fetch)
-	    You can do things with the database, and return 
-	    the results. 
-	    Basically, urls.py tells the server what to do 
-	    with a given request. There, you can tell the 
-	    server: "whenever you receive a url that ends in /contact/aService",
-	     come here, and call this function.
-	    The function receives the request as an argument! 
-	    Isn't Django nice? It can also be passed arguments,
-	    like the net_id that was parsed from the URL. 
-	    The function then returns something, and that's 
-	    what you'll receive in your javascript environment
-	    when the fetch call (or whatever ajax call you use)
-	    returns. 
-	"""
+	return JsonResponse({'success': 'true'})
 
 	try:
 		return JsonResponse({'body': request})
-		
+
 		content = request.body
 		cond_and_id = content['conditionAndId']
 		final_result = content['finalResult']
